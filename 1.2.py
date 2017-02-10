d9db13fe661968cf9f75e86811a00b693076dbf7
Wing lee <mmmwhy@qq.com>
2017-02-10 17:25:34 +0800

    使用 Python 生成 200 个激活码

---
+++ 1.2.py
@@ -0,0 +1,16 @@
+import random
+import string
+FIELD = string.digits + string.letters
+def generate(n, many=1, where=None):
+    def getCode(n):
+        return "".join(random.sample(FIELD, n))
+    gene = [getCode(n) for i in range(many)]
+    return gene
+def writeIn(n, many, where):
+    count = 1
+    for i in generate(n, many):
+        with open(where, "a") as boom:
+            boom.write(str(count).rjust(3)+"  "+i+"\n")
+        count += 1
+if __name__ == '__main__':
+    writeIn(20, 200, "coupon.txt")
