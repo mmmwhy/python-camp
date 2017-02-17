ef0ca5ca6a592ec71b0ad08818a332dbba6094f2
Wing lee <mmmwhy@qq.com>
2017-02-17 17:19:43 +0800

    Python对密码加密

---
+++ 1.9(py2).py
@@ -0,0 +1,25 @@
+import os,base64
+from hashlib import sha256
+from hmac import HMAC
+def encrypt_password(password, salt=None):
+    """Hash password on the fly."""
+    if salt is None:
+        salt = os.urandom(8) # 64 bits.
+    assert 8 == len(salt)
+    assert isinstance(salt, str)
+    if isinstance(password, unicode):
+        password = password.encode('UTF-8')
+    assert isinstance(password, str)
+    result = password
+    for i in xrange(10):
+        result = HMAC(result, salt, sha256).digest()
+    return salt + result
+def validate_password(hashed, input_password):
+    return hashed == encrypt_password(input_password, salt=hashed[:8])
+    
+if __name__=="__main__":
+    hashed = encrypt_password('secret password')
+    assert validate_password(hashed, 'secret password')
+    print (hashed)
+    print (base64.b64encode(hashed))
+    print (base64.b64decode(base64.b64encode(hashed)))
